b1c7fde7751fb01613765c0d637cb22a059097e3
Priyadharshan <rkdharshan007@gmail.com>
2026-03-11 09:22:45 +0530

    uc2

---
+++ uc2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class uc2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        name = scanner.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
